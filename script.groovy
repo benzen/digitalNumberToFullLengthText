ec5b7d3f323771610993d53645fa2b0ed2b1ab97
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-01-27 23:00:17 -0500

    remove optional one

--- script.groovy
+++ script.groovy
@@ -5,9 +5,9 @@ def countThousands ={number -> ((number/1000) as int) % 1000}
 def countMillions = {number -> ((number/1000000)as int )% 1000}
 def countBillions = {number -> ((number/1000000000)as int )% 1000}
 
-def numberToWord =  [ 0:"zero", 1:"un", 2:"deux",3:"trois",4:"quatre",5:"cinq", 6:"six",7:"sept",8:"huit",9:"neuf",                                         10:"dix",11:"onze",12:"douze",13:"treize",14:"quatorze",15:"quinze",16:"seize",
-                      20:"vingt",30:"trente",40:"quarante",50:"cinquante",60:"soixante",
-                      70:"soixante-dix",80:"quatre-vingt",90:"quatre-vingt-dix", 100:"cent"]
+def numberToWord =  [ 0:"zero", 1:"un", 2:"deux", 3:"trois", 4:"quatre", 5:"cinq", 6:"six", 7:"sept", 8:"huit", 9:"neuf",                                         			
+					  10:"dix",11:"onze",12:"douze",13:"treize",14:"quatorze",15:"quinze",16:"seize",
+                      20:"vingt",30:"trente",40:"quarante",50:"cinquante",60:"soixante", 70:"soixante-dix",80:"quatre-vingt",90:"quatre-vingt-dix", 100:"cent"]
 def upperNumberToWord = [1000:"mille",1000000:"million"]
 
 def numberToString = {counted, multiple = null->
@@ -16,9 +16,9 @@ def numberToString = {counted, multiple = null->
     if(multiple && counted) return "${numberToWord.get(counted)} $multiple"
     return  numberToWord.get(counted)
 }
-def coumpondNumberToString = { counted, multiple = null, optionalOne=false->
+def coumpondNumberToString = { counted, multiple = null->
     if(!counted) return ""
-    if( optionalOne && multiple  && ( counted == numberToWord.get(1) ) ) return multiple 
+    if( multiple  && (multiple != upperNumberToWord.get(1000)) && ( counted == numberToWord.get(1) ) ) return multiple 
     if(multiple && counted) return "$counted $multiple"
     return  numberToWord.get(counted)
 }
@@ -38,7 +38,7 @@ def convert = { number ->
     def millionsAsString = numberStringToString(convertUpToThousand(millions), upperNumberToWord.get(1000000))
 
     def thousands = countThousands(number) 
-    def thousandsAsString = numberStringToString( convertUpToThousand(thousands), upperNumberToWord.get(1000), true )
+    def thousandsAsString = numberStringToString( convertUpToThousand(thousands), upperNumberToWord.get(1000) )
     
     builder.append(millionsAsString)
     builder.append(" ")
